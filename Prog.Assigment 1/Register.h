ade020c4017b06d96b5c05555145545a89871426
YayItsThunder <szcsyy@126.com>
2022-04-03 14:52:52 +0800

    header and csv

---
+++ Prog.Assigment 1/Register.h
@@ -0,0 +1,24 @@
+//
+// Register.h Created by YayItsThunder on 2022.4.3
+//
+
+#include <stdio.h>
+#include <iostream>
+#include <cstdlib>
+#include <string>
+using namespace std;
+class Person
+{
+    public:
+        int id;
+        string name;
+        int profession;
+        int risk;
+        string birth;
+        int age;
+        string address;
+        int phone;
+        int wechat;
+        string email;
+
+};

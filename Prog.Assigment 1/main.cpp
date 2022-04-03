b8ca32c6583070c75ffc09e5107afb35c7e12655
xinzhuo20 <xinzhuo.20@intl.zju.edu.cn>
2022-04-03 15:41:32 +0800

    laofu

--- Prog.Assigment 1/main.cpp
+++ Prog.Assigment 1/main.cpp
@@ -1,1 +1,20 @@
 //CA1 Group D2
+
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <cstdlib>
+using namespace std;
+//Include.h files and other .cpp files here
+
+//weekly report:Treated people；Registered people with appointment；Queueing people without appointments
+//(Including prof+age+risk+time)
+int report_weekly (int)
+//monthly report:Number of registered people; Number of waiting people (already in register); Number of appointments
+
+//------------------------------main function------------------------------
+//1. Patients input
+
+//2. Deal with queues and lists
+
+//3. Report: Both weekly and monthly

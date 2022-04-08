b61a23e3939162714c2b78252ccc75229f847047
YayItsThunder <szcsyy@126.com>
2022-04-08 10:37:09 +0800

    modified

--- Prog.Assigment 1/Register.cpp
+++ Prog.Assigment 1/Register.cpp
@@ -22,14 +22,13 @@ person::person(void)
     email = "";
     register_order = -1;
     register_day = -1;
-    to_ddl = -1;
     preferred_hos1 = -1;
     preferred_hos2 = -1;
     preferred_hos3 = -1;
     apponitment_loc = -1;
     treated_order = -1;
+    treated_date = -1;
     treated_time ="";
-    treated_date ="";
     if_treated = false;
     if_appointed = false;
     if_queueing = false;

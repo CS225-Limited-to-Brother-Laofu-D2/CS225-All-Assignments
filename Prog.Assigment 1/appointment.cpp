96b09dce6221fe7aa68830ef1594bb038283a146
xinzhuo20 <xinzhuo.20@intl.zju.edu.cn>
2022-04-08 12:04:49 +0800

    cr

--- Prog.Assigment 1/appointment.cpp
+++ Prog.Assigment 1/appointment.cpp
@@ -31,6 +31,135 @@ appointment::appointment(/* args */)
     }
 }
 
+void appointment::agesort()
+{
+    // get the length of the queue p
+    int len=p->size();
+    person a[len]; // use a[] to store reg node from the queue
+    // char b[len];
+    person *p1;
+    person *p2;
+
+    // store and pop every element from the queue
+    for (int i = 0; i < len; ++i)
+    {
+        a[i] = p->front();
+        p->pop();
+    }
+
+    // use selection sort to sort based on the age
+    for (int i =0; i < len; ++i)
+    {
+        int max=i;
+        for (int j=i+1; j< len; ++j)
+        {
+            p1 = &a[max];
+            p2 = &a[j];
+
+            // if p1 larger than p2, swap them
+            if (p1->get_age() < p2->get_age())
+            {
+                max=j;
+            }
+        }
+        person tmp = a[i];
+        a[i] = a[max];
+        a[max] = tmp;
+    }
+    for (int i=0; i< len; ++i)
+    {
+        p->push(a[i]);
+    }
+    // return p;
+}
+
+// sorting according to name
+void appointment::namesort()
+{
+    int len=p->size();
+    person a[len];
+    // char b[len];
+    person *p1;
+    person *p2;
+
+    for (int i = 0; i < len; ++i)
+    {
+        a[i] = p->front();
+        p->pop();
+    }
+
+    // use selection sort to sort based on the name
+    for (int i =0; i < len; ++i)
+    {
+        int max=i;
+        for (int j=i+1; j< len; ++j)
+        {
+            p1 = &a[max];
+            p2 = &a[j];
+
+            // if p1 larger than p2, swap them
+            if(p1->get_name() < p2->get_name())
+            {
+                max=j;
+            }
+        }
+        person tmp = a[i];
+        a[i] = a[max];
+        a[max] = tmp;
+    }
+
+    for (int i=0; i< len; ++i)
+    {
+        p->push(a[i]);
+    }
+    // return p;
+}
+
+
+// sort according to profession
+void appointment::profsort()
+{
+    // get the length of the queue p
+    int len=p->size();
+    person a[len]; // use a[] to store reg node from the queue
+    // char b[len];
+    person *p1;
+    person *p2;
+
+    // store and pop every element from the queue
+    for (int i = 0; i < len; ++i)
+    {
+        a[i] = p->front();
+        p->pop();
+    }
+
+    // use selection sort to sort based on the profession
+    for (int i =0; i < len; ++i)
+    {
+        int max=i;
+        for (int j=i+1; j< len; ++j)
+        {
+            p1 = &a[max];
+            p2 = &a[j];
+
+            // if p1 larger than p2, swap them
+            if (p1->get_profession() < p2->get_profession())
+            {
+                max=j;
+            }
+        }
+        person tmp = a[i];
+        a[i] = a[max];
+        a[max] = tmp;
+    }
+    for (int i=0; i< len; ++i)
+    {
+        p->push(a[i]);
+    }
+    // return p;
+}
+
+
 
 void set_appointment(person *one,appointment *day){
     int pos_left=day->get_num();

dc615781d5f493ebf3227cde65ddb09cca9eb1aa
YayItsThunder <szcsyy@126.com>
2022-04-08 14:42:59 +0800

    Roughly Finished

--- Prog.Assigment 1/Register.cpp
+++ Prog.Assigment 1/Register.cpp
@@ -3,6 +3,7 @@
 //
 
 #include "Register.h"
+#include "FibHeap.h"
 
 // Return 0 means cannot open this file, retry is needed.
 // Return 1 means open successfully.
@@ -39,26 +40,33 @@ person::person(void)
 int local_queue1_push_pop(int k , int register_process , person** input_array , Centralized_Queue<person*> Central_queue , queue<person*> localqueue_1 , queue<person*> localqueue_1_medium_risk , queue<person*> localqueue_1_high_risk)
 {
     int day = (k + 1) / 2;
-    int counter ; // Every half day we only process 10 people.
-    while(!localqueue_1_medium_risk.empty() && localqueue_1_medium_risk.front()->wait_before_in_queue == day)
+    int counter = 0; // Every half day we only process 10 people.
+    while(!localqueue_1_medium_risk.empty() && localqueue_1_medium_risk.front()->wait_before_in_queue == day && counter <= 10)
     {
         person* medium_person_process = localqueue_1_medium_risk.front();
         localqueue_1_medium_risk.pop(); // We found a meidum risk people that need to push into the local queue.
         localqueue_1.push(medium_person_process);
         medium_person_process->if_queueing = true;
         medium_person_process->register_day = day;
-        medium_person_process->register_day = day + 20;
+        medium_person_process->ddl_day = day + 20;
         counter++;
-        register_process++;
     }
-    
-    if(Central_queue.fib_heap)
+    if(Central_queue.fib_heap->ifempty()) // In this case, we can process with the people with high risk.
     {
-
+        while(!localqueue_1_high_risk.empty() && counter <= 10)
+        {
+            person* high_person_process = localqueue_1_high_risk.front();
+            localqueue_1_high_risk.pop();
+            localqueue_1.push(high_person_process);
+            high_person_process->if_queueing = true;
+            high_person_process->register_day = day;
+            high_person_process->ddl_day = day + 20;
+            counter++;
+        }
     }
     for(/*nothing here*/; counter <= 10 ; counter++)
     {
-        person* person_now_process = input_array[register_process + counter];
+        person* person_now_process = input_array[register_process];
         if(person_now_process->risk == 0 || person_now_process->risk == 1)// no or low risk
         {
             localqueue_1.push(person_now_process);
@@ -73,7 +81,7 @@ int local_queue1_push_pop(int k , int register_process , person** input_array , 
             {
                 person_now_process->wait_before_in_queue = day + 30;
                 localqueue_1_medium_risk.push(person_now_process);
-                counter--; // We didnt process people this time.
+                counter--; // We didn't push people into queue this time.
                 register_process++;
             }
             else
@@ -81,7 +89,7 @@ int local_queue1_push_pop(int k , int register_process , person** input_array , 
                 if(person_now_process->risk == 3) // Hight risk
                 {
                     localqueue_1_high_risk.push(person_now_process);
-                    counter--; // We didnt process people this time.
+                    counter--; // We didn't push people into queue this time.
                     register_process++;
                 }
                 else
@@ -97,7 +105,7 @@ int local_queue1_push_pop(int k , int register_process , person** input_array , 
     {
         person* person_push_into = localqueue_1.front();
         localqueue_1.pop();
-        
+        Central_queue.record_in(person_push_into);
     }
     return register_process;
 }

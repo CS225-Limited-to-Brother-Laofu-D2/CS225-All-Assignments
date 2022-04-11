9a0a179efbb1de50b09e96f590a1256901e905bf
YayItsThunder <szcsyy@126.com>
2022-04-11 18:47:04 +0800

    d

--- Our midterm/Yangbao/Yangbao/Ex8_Stacks_template/stacks_template.cpp
+++ Our midterm/Yangbao/Yangbao/Ex8_Stacks_template/stacks_template.cpp
@@ -64,7 +64,7 @@ template<class T> void Stacks<T>::allocate(void)
     // This function needs to be modified
     int newsize = 2 * maxsize;
     T *newarray = new T[newsize];
-    for (int i = 0; i < numitems; ++i)
+    for (int i = 0; i < numitems1 + numitems2; ++i)
     {
         newarray[i] = reprarray[i];
     }
@@ -97,7 +97,7 @@ template<class T> void Stacks<T>::deallocate(void)
     // This function needs to be modified
     int newsize = maxsize / 2;
     T *newarray = new T[newsize];
-    for (int i = 0; i < numitems; ++i)
+    for (int i = 0; i < numitems1 + numitems2; ++i)
     {
         newarray[i] = reprarray[i];
     }
@@ -110,44 +110,94 @@ template<class T> void Stacks<T>::deallocate(void)
 template<class T> T Stacks<T>::top1(void)
 {
     // This function needs to be implemented
+    int stack_length = this->getlength1();
+    return this->reprarray[stack_length];
 }
 
 template<class T> T Stacks<T>::top2(void)
 {
     // This function needs to be implemented
+    int stack_length = this->getlength2();
+    return this->reprarray[maxsize - stack_length];
 }
 
 template<class T> T Stacks<T>::pop1(void)
 {
     // This function needs to be implemented
+    int stack_length = this->getlength1();
+    numitems1--;
+    return this->reprarray[stack_length];
 }
 
 template<class T> T Stacks<T>::pop2(void)
 {
     // This function needs to be implemented
+    int stack_length = this->getlength2();
+    numitems2--;
+    return this->reprarray[maxsize - stack_length + 1];
 }
 
 template<class T> void Stacks<T>::push1(T item)
 {
     // This function needs to be implemented
+    if(maxsize == numitems1 + numitems2)
+    {
+        allocate();
+    }
+    int stack_length = this->getlength1();
+    reprarray[stack_length + 1] = item;
+    numitems1++;
 }
 
 template<class T> void Stacks<T>::push2(T item)
 {
     // This function needs to be implemented
+    if(maxsize == numitems1 + numitems2)
+    {
+        allocate();
+    }
+    int stack_length = this->getlength2();
+    reprarray[maxsize - stack_length] = item;
+    numitems2++;
 }
 
 template<class T> bool Stacks<T>::isempty1(void)
 {
     // This function needs to be implemented
+    if(numitems1 == 0)
+    {
+        return true;
+    }
+    else
+    {
+        return false;
+    }
 }
 
 template<class T> bool Stacks<T>::isempty2(void)
 {
     // This function needs to be implemented
+    if(numitems2 == 0)
+    {
+        return true;
+    }
+    else
+    {
+        return false;
+    }
 }
 
 template<class T> void Stacks<T>::display(void)
 {
     // This function needs to be implemented
+    cout<<"In stack 1 we have (from top of the stack to the bottom of the stack)\n";
+    for(int i = this->getlength1() ; i >= 1 ; i--)
+    {
+        cout<<reprarray[i]<<"\n";
+    }
+    cout<<"In stack 2 we have (from top of the stack to the bottom of the stack)\n";
+    for(int i = maxsize - this->getlength2() + 1 ; i <= maxsize  ; i++)
+    {
+        cout<<reprarray[i]<<"\n";
+    }
 }
